493327e87ef3ebabc054eeb74a664aff7ecb35df
Shocktail <MVboy39@gmail.com>
2019-04-25 19:04:30 -0400

    more handling bad input better

--- src/main/java/shocktail/kunnecktphoure/KunnecktPhoure.java
+++ src/main/java/shocktail/kunnecktphoure/KunnecktPhoure.java
@@ -17,27 +17,39 @@ import javax.swing.JOptionPane;
 public class KunnecktPhoure {
 	private static K4Panel panel = null;
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args) throws IOException {
 		Object o = JOptionPane.showInputDialog(null, "local or online?", "kunneckt phoure", JOptionPane.DEFAULT_OPTION,
 				null, new String[] { "local", "host", "join", "exit" }, "0");
 		if ("local".equals(o)) {
 			KunnecktPhoure.panel = new K4Panel();
 		} else if ("host".equals(o)) {
-				try {
-					KunnecktPhoure.panel = new NetplayGameP1(
-							Integer.parseInt(JOptionPane.showInputDialog("port to listen on")));
-				} catch (NumberFormatException ex) {
-					JOptionPane.showMessageDialog(null, "bad input, exiting");
-					System.exit(0);
-				}
-		} else if ("join".equals(o)) {
-			String s = JOptionPane.showInputDialog("ip:port");
 			try {
-				KunnecktPhoure.panel = new NetplayGameP2(InetAddress.getByName(s.substring(0, s.indexOf(":"))),
-						Integer.parseInt(s.substring(s.indexOf(":") + 1)));
+				int port = Integer.parseInt(JOptionPane.showInputDialog("port to listen on"));
+				if (port < 0 || port > 65535) {
+					JOptionPane.showMessageDialog(null, "bad input, exiting");
+					return;
+				}
+				KunnecktPhoure.panel = new NetplayGameP1(port);
 			} catch (NumberFormatException ex) {
 				JOptionPane.showMessageDialog(null, "bad input, exiting");
-				System.exit(0);
+				return;
+			}
+		} else if ("join".equals(o)) {
+			String s = JOptionPane.showInputDialog("ip:port");
+			if (!s.matches("(([0-9]{1,3}\\.){3})[0-9]{1,3}:[0-9]{1,5}")) {
+				JOptionPane.showMessageDialog(null, "bad input, exiting");
+				return;
+			}
+			try {
+				int port = Integer.parseInt(s.substring(s.indexOf(":") + 1));
+				if (port < 0 || port > 65535) {
+					JOptionPane.showMessageDialog(null, "bad input, exiting");
+					return;
+				}
+				KunnecktPhoure.panel = new NetplayGameP2(InetAddress.getByName(s.substring(0, s.indexOf(":"))), port);
+			} catch (UnknownHostException ex) {
+				JOptionPane.showMessageDialog(null, "bad input, exiting");
+				return;
 			}
 		} else {
 			return;
